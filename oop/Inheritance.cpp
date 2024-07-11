5e224acda8eed387b93fde65678d296b42165ff9
ShikhaPallavi123 <shikhapallavi10389@gmail.com>
2024-07-10 21:11:54 -0700

    practice inheritance class

--- oop/Inheritance.cpp
+++ oop/Inheritance.cpp
@@ -1,3 +1,32 @@
 //
 // Created by Shikha Pallavi on 7/10/24.
 //
+#include<iostream>
+using namespace std;
+
+class A{
+public:
+    int a = 0;
+    int b = 1;
+    void func(){
+        cout << "class A: " << endl;
+    }
+
+};
+
+class B : public A{
+public:
+    int p = 0;
+    int q = 1;
+    void function(){
+        cout << "class B: " << endl;
+    }
+
+};
+
+int main(){
+
+    B b1;
+    b1.func();
+
+}
