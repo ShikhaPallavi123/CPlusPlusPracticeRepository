818ef82489242c505dad85ef5a8cef37afb30490
ShikhaPallavi123 <shikhapallavi10389@gmail.com>
2024-07-12 20:14:36 -0700

    Add factorial recursive solution

--- datastructure/RecursionBasics.cpp
+++ datastructure/RecursionBasics.cpp
@@ -1,3 +1,18 @@
 //
 // Created by Shikha Pallavi on 7/12/24.
 //
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+    if(n == 0){
+        return 1;
+    }
+     return n * factorial(n-1);
+}
+
+int main(){
+    int ans = factorial(4);
+    cout << "factorial of n is: " << ans << endl;
+    return 0;
+}
