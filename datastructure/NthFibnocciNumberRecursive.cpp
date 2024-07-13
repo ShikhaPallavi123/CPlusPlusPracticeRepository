d5e0b4cb38891c439dd7e9a5d2b33be1fb8a209f
ShikhaPallavi123 <shikhapallavi10389@gmail.com>
2024-07-12 21:00:56 -0700

    Add Nth fibnocci Number solution recursively

---
+++ datastructure/NthFibnocciNumberRecursive.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Shikha Pallavi on 7/12/24.
+//
+#include<iostream>
+using namespace std;
+
+int fibonacci(int n){
+    if(n == 0 || n == 1) {
+        return n;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+
+}
+
+int main(){
+    cout << fibonacci(5) << endl;
+    return 0;
+}
